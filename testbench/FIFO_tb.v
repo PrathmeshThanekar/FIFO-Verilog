f0cf910c88e9d7acdb25cf1c11113e3ca3dc5a50
Prathmesh Thanekar <pthanekar26@gmail.com>
2026-08-13 11:01:06 +0530

    Add files via upload

--- testbench/FIFO_tb.v
+++ testbench/FIFO_tb.v
@@ -1,1 +1,65 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/05/2026 08:17:54 PM
+// Design Name: 
+// Module Name: FIFO_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module FIFO_tb();
+    reg clk, rst;
+    reg [7:0] data_in;
+    wire [7:0] data_out;
+    
+    
+    FIFO_Top  uut(clk, rst, data_in, data_out);
+    
+    always begin 
+    # 5 clk = ~ clk;
+    end
+    initial 
+    begin 
+    {clk, rst, data_in} = 0; 
+    #10;
+    rst = 1;
+    #10;
+    rst = 0;
+    #10;
+    data_in = 1;
+    #10;
+    data_in = 2;
+    #10;
+    data_in = 3;
+    #10;
+    data_in = 4;
+    #10;
+    data_in = 5;
+    #10;
+    data_in = 6;
+    #10;
+    data_in = 7;
+    #10;
+    data_in = 8;
+    #10;
+    data_in = 9;
+    #10;
+    data_in = 10;
+    
+    #60;
+    $stop;
+    end
+endmodule
